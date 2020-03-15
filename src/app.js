86d2e8d963d6d0a2534dcb3d412fb3069800edf9
Aichi Chang <phoebexxxc@gmail.com>
2020-03-15 21:45:12 +0000

    basic functionality

--- src/app.js
+++ src/app.js
@@ -1,5 +1,25 @@
 function main() {
 
+  const alcoholPercentage =  document.getElementById('alcoholPercent')
+  const alcoholVolume = document.getElementById('alcoholVolume')
+  const outputTag = document.getElementById('output')
+  const totalTag = document.getElementById('total')
+  const buttonTag = document.getElementById('button')
+
+  buttonTag.addEventListener('click', () => {
+
+    const x = ((alcoholPercentage.value * alcoholVolume.value) / 0.75) - alcoholVolume.value
+    console.log(x)
+    const y = x + parseFloat(alcoholVolume.value)
+    outputTag.innerHTML = x
+    totalTag.innerHTML = y
+    // console.log(typeof(alcoholVolume.value))
+  })
+
+  
+
+
+
 
 
 
