970b05e200acd3e07848dffac4b17a397a19374d
Aichi Chang <phoebexxxc@gmail.com>
2020-03-16 22:11:53 +0000

    fixed bug

--- src/app.js
+++ src/app.js
@@ -6,29 +6,40 @@ function main() {
   const totalTag = document.getElementById('total')
   const buttonTag = document.getElementById('button')
   const waterTag = document.getElementById('water')
+  const lowerThan75Tag = document.getElementById('lowerThan75')
 
 
 
   alcoholPercentage.addEventListener('change', (input) => {    
     if (input.target.valueAsNumber < 0) input.target.valueAsNumber = 0
-    if (input.target.valueAsNumber > 1) input.target.valueAsNumber = 1 
+    if (input.target.valueAsNumber > 100) input.target.valueAsNumber = 100 
     // console.log(input.target.valueAsNumber)
   })
 
+  alcoholVolume.addEventListener('change', (input) => {    
+    if (input.target.valueAsNumber < 0) input.target.valueAsNumber = 0
+    // console.log(input.target.valueAsNumber)
+  })
+  
   
   
   buttonTag.addEventListener('click', () => {
 
-    const x = ((alcoholPercentage.value * alcoholVolume.value) / 0.75) - alcoholVolume.value
-    // console.log(x)
+    const x = (((alcoholPercentage.value / 100) * alcoholVolume.value) / 0.75) - alcoholVolume.value
     const y = x + parseFloat(alcoholVolume.value)
-    waterTag.innerHTML = Math.floor(x)
-    totalTag.innerHTML = Math.floor(y)
-    if (x && y) {
-      outputTag.style.visibility = 'visible'
-    }
 
-   
+    if (alcoholPercentage.value < 75) {
+      outputTag.style.visibility = 'hidden'
+      lowerThan75Tag.style.visibility = 'visible'
+      lowerThan75Tag.innerHTML = 'The alcohol percentage is lower than 75%'
+    } else if (x && y) {
+      lowerThan75Tag.style.visibility = 'hidden'
+      outputTag.style.visibility = 'visible'
+      waterTag.innerHTML = Math.floor(x)
+      totalTag.innerHTML = Math.floor(y)
+    } 
+
+
     // console.log(typeof(alcoholVolume.value))
   })
 
