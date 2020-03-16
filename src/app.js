3e9a3a90b676b497d214d3276c32162412dd69ac
Aichi Chang <phoebexxxc@gmail.com>
2020-03-16 21:55:07 +0000

    limit input number

--- src/app.js
+++ src/app.js
@@ -1,27 +1,34 @@
 function main() {
 
   const alcoholPercentage =  document.getElementById('alcoholPercent')
-  const percentageTag = document.getElementById('percentage')
   const alcoholVolume = document.getElementById('alcoholVolume')
   const outputTag = document.getElementById('output')
   const totalTag = document.getElementById('total')
   const buttonTag = document.getElementById('button')
+  const waterTag = document.getElementById('water')
 
 
 
-  // percentageTag.addEventListener('change', (input) => {    
-  //   if (input < 0) input = 0
-  //   if (input > 100) input = 100
-  // })
+  alcoholPercentage.addEventListener('change', (input) => {    
+    if (input.target.valueAsNumber < 0) input.target.valueAsNumber = 0
+    if (input.target.valueAsNumber > 1) input.target.valueAsNumber = 1 
+    // console.log(input.target.valueAsNumber)
+  })
+
   
-
+  
   buttonTag.addEventListener('click', () => {
 
     const x = ((alcoholPercentage.value * alcoholVolume.value) / 0.75) - alcoholVolume.value
-    console.log(x)
+    // console.log(x)
     const y = x + parseFloat(alcoholVolume.value)
-    outputTag.innerHTML = x
-    totalTag.innerHTML = y
+    waterTag.innerHTML = Math.floor(x)
+    totalTag.innerHTML = Math.floor(y)
+    if (x && y) {
+      outputTag.style.visibility = 'visible'
+    }
+
+   
     // console.log(typeof(alcoholVolume.value))
   })
 
